6b4eba148d508edb0ec2c9454b87f5c088686cc0
Orisunmibare Muiz <orismuiz2003@gmail.com>
2024-09-12 18:48:33 +0100

    Upload🤗

---
+++ app.js
@@ -0,0 +1,27 @@
+var Input = document.getElementById('greetings');
+var greetBtn = document.getElementById('btn');
+window.alert("I'll greet your ancestor!");
+
+function greet() {
+    greetBtn.addEventListener('click', () => {
+        let currentDate = new Date();
+        let currentHour = currentDate.getHours()
+        var greetingMessage;
+        
+        
+        
+        if (currentHour >= 0 && currentHour < 12 ) {
+            greetingMessage = "Good morning 🌄";
+        } else if (currentHour >= 12 && currentHour < 18){ 
+            greetingMessage = "Good afternoon 🌞"
+        } else if (currentHour >= 18 && currentHour < 24) {
+            greetingMessage = "Good evening 🌆"
+        } else {
+            greetingMessage = ""
+        }
+        
+        Input.value = greetingMessage
+    })
+}
+greet();
+
